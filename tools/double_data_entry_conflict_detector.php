3f77d2a46138cb6a605f99d48f9b89dd7ed5c061
samir <samir>
2009-11-03 20:29:17 +0000

    added double data entry feature

---
+++ tools/double_data_entry_conflict_detector.php
@@ -0,0 +1,48 @@
+<?
+
+/*
+Compare tsting script
+*/
+
+set_include_path(get_include_path().":../../php/libraries:");
+require_once "NDB_Client.class.inc";
+
+$client = new NDB_Client();
+$client->makeCommandLine();
+$client->initialize('../config.xml');
+require_once "ConflictDetector.class.inc";
+
+$db =& Database::singleton();
+
+
+$instrument_name='head_measurements_subject';
+
+
+$instance_pairs = getInstancePairsForInstrument($instrument_name);
+
+require_once "../../project/instruments/NDB_BVL_Instrument_".$instrument_name.".class.inc";
+
+foreach($instance_pairs AS $instance_pair) {
+    print "Checking $instrument_name $instance_pair[0]\n";
+    ConflictDetector::clearConflictsForInstance($instance_pair[0]);
+    $diff=ConflictDetector::detectConflictsForCommentIds($instrument_name, $instance_pair[0], $instance_pair[1]);
+    ConflictDetector::recordUnresolvedConflicts($diff);
+}
+
+
+function getInstancePairsForInstrument($instrument_name) {
+    $db =& Database::singleton();
+
+    $query = "SELECT CommentID, CONCAT('DDE_', CommentID) AS DDECommentID FROM flag where Test_name='$instrument_name' and LEFT(CommentID, 3) != 'DDE'";
+    $db->select($query, $instances);
+
+    $instance_pairs = array();
+
+    foreach($instances AS $instance) {
+        $instance_pairs[] = array($instance['CommentID'], $instance['DDECommentID']);
+    }
+
+    return $instance_pairs;
+}
+
+?>
