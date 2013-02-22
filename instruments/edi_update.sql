ee28a744bf5c406c251efef776ef529894b15e5b
Rathi Sekaran <sekaranrathi@gmail.com>
2013-02-22 13:55:49 -0500

    removing color next to div

--- instruments/edi_update.sql
+++ instruments/edi_update.sql
@@ -35,7 +35,7 @@ UPDATE edi SET timeframe_visit = "0-6m" , hosp = hosp_0_6months WHERE hosp_0_6mo
 UPDATE edi SET timeframe_visit = "0-6m" , mill = mill_0_6months WHERE mill_0_6months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "7m-12m" , bir = bir_7_12months WHERE bir_7_12months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "7m-12m" , dea = dea_7_12months WHERE dea_7_12months IS NOT NULL;
-UPDATE edi SET timeframe_visit = "7m-12m" , div: = div_7_12months WHERE div_7_12months IS NOT NULL;
+UPDATE edi SET timeframe_visit = "7m-12m" , div = div_7_12months WHERE div_7_12months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "7m-12m" , mar = mar_7_12months WHERE mar_7_12months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "7m-12m" , mov = mov_7_12months WHERE mov_7_12months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "7m-12m" , tem = tem_7_12months WHERE tem_7_12months IS NOT NULL;
@@ -50,7 +50,7 @@ UPDATE edi SET timeframe_visit = "7m-12m" , hosp = hosp_7_12months WHERE hosp_7_
 UPDATE edi SET timeframe_visit = "7m-12m" , mill = mill_7_12months WHERE mill_7_12months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-18m" , bir = bir_13_18months WHERE bir_13_18months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-18m" , dea = dea_13_18months WHERE dea_13_18months IS NOT NULL;
-UPDATE edi SET timeframe_visit = "13m-18m" , div: = div_13_18months WHERE div_13_18months IS NOT NULL;
+UPDATE edi SET timeframe_visit = "13m-18m" , div = div_13_18months WHERE div_13_18months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-18m" , mar = mar_13_18months WHERE mar_13_18months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-18m" , mov = mov_13_18months WHERE mov_13_18months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-18m" , tem = tem_13_18months WHERE tem_13_18months IS NOT NULL;
@@ -65,7 +65,7 @@ UPDATE edi SET timeframe_visit = "13m-18m" , hosp = hosp_13_18months WHERE hosp_
 UPDATE edi SET timeframe_visit = "13m-18m" , mill = mill_13_18months WHERE mill_13_18months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-24m" , bir = bir_13_24months WHERE bir_13_24months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-24m" , dea = dea_13_24months WHERE dea_13_24months IS NOT NULL;
-UPDATE edi SET timeframe_visit = "13m-24m" , div: = div_13_24months WHERE div_13_24months IS NOT NULL;
+UPDATE edi SET timeframe_visit = "13m-24m" , div = div_13_24months WHERE div_13_24months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-24m" , mar = mar_13_24months WHERE mar_13_24months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-24m" , mov = mov_13_24months WHERE mov_13_24months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "13m-24m" , tem = tem_13_24months WHERE tem_13_24months IS NOT NULL;
@@ -80,7 +80,7 @@ UPDATE edi SET timeframe_visit = "13m-24m" , hosp = hosp_13_24months WHERE hosp_
 UPDATE edi SET timeframe_visit = "13m-24m" , mill = mill_13_24months WHERE mill_13_24months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "19m-24m" , bir = bir_19_24months WHERE bir_19_24months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "19m-24m" , dea = dea_19_24months WHERE dea_19_24months IS NOT NULL;
-UPDATE edi SET timeframe_visit = "19m-24m" , div: = div_19_24months WHERE div_19_24months IS NOT NULL;
+UPDATE edi SET timeframe_visit = "19m-24m" , div = div_19_24months WHERE div_19_24months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "19m-24m" , mar = mar_19_24months WHERE mar_19_24months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "19m-24m" , mov = mov_19_24months WHERE mov_19_24months IS NOT NULL;
 UPDATE edi SET timeframe_visit = "19m-24m" , tem = tem_19_24months WHERE tem_19_24months IS NOT NULL;
