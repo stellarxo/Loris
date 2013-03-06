10a76a016f9c104f40d0e27b2475a0f99dcda290
Rathi Sekaran <sekaranrathi@gmail.com>
2013-03-06 17:06:37 -0500

    updating table with new fields

---
+++ instruments/figs_update.sql
@@ -0,0 +1,3 @@
+alter table figs_year3_relatives add Face_fragilex_syndrome enum('0','1','9','NA');
+alter table figs_year3_relatives add Face_outcome_test enum('1','2','3','4','5','6','7','8','9');
+alter table figs_year3_relatives add Face_testoutcome_other text;
