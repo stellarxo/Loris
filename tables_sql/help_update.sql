72734e05ddf0bf18aad695c6c1ca16d35817ab6f
Rathi Sekaran <sekaranrathi@gmail.com>
2013-03-18 12:00:00 -0400

    sql statement to add section to help

---
+++ tables_sql/help_update.sql
@@ -0,0 +1,6 @@
+INSERT into help (hash, topic, content,created) VALUES (md5('HandPreference'),'Hand Preference','Under Construction.Please visit us later',CURDATE());
+INSERT into help (hash, topic, content,created) VALUES (md5('ados2_module1'),'ADOS2 Module1','Under Construction.Please visit us later',CURDATE());
+INSERT into help (hash, topic, content,created) VALUES (md5('ados2_module2'),'ADOS2 Module2','Under Construction.Please visit us later',CURDATE());
+INSERT into help (hash, topic, content,created) VALUES (md5('ados2_module3'),'ADOS2 Module3','Under Construction.Please visit us later',CURDATE());
+INSERT into help (hash, topic, content,created) VALUES (md5('ECBQ'),'Early Childhood Behavior Questionnaire(ECBQ)','Under Construction.Please visit us later',CURDATE());
+
