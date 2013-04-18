68b706310ff5f828983d09f3ffcd7d60688deb6f
Rathi Sekaran <sekaranrathi@gmail.com>
2013-04-18 15:14:55 -0400

    adding sql script for lookup table

---
+++ tables_sql/fes_lookup.sql
@@ -0,0 +1,214 @@
+CREATE TABLE `fes_lookup` (
+        `Rawscore` int(11) NOT NULL,
+        `Achievement` varchar(11) DEFAULT NULL,
+        `Percentile` varchar(255) DEFAULT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'c',65);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'ex',71);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'con',80);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'ind',69);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'ao',72);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'ico',69);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'aro',69);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'mre',71);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'org',69);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (9,'ctl',76
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'c',62);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'ex',68);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'con',78);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'ind',65);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'ao',69);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'ico',66);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'aro',66);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'mre',68);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'org',66);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8.5,'ctl',73
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'c',59);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'ex',65);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'con',75);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'ind',61);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'ao',66);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'ico',63);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'aro',64);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'mre',66);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'org',63);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (8,'ctl',70
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'c',55);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'ex',62);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'con',73);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'ind',57);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'ao',63);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'ico',61);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'aro',61);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'mre',64);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'org',61);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7.5,'ctl',68
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'c',52);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'ex',59);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'con',70);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'ind',53);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'ao',59);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'ico',58);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'aro',59);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'mre',61);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'org',58);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (7,'ctl',65
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'c',48);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'ex',56);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'con',67);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'ind',49);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'ao',56);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'ico',55);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'aro',56);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'mre',59);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'org',55);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6.5,'ctl',62
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'c',45);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'ex',53);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'con',65);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'ind',45);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'ao',53);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'ico',52);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'aro',53);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'mre',56);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'org',53);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (6,'ctl',59
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'c',42);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'ex',50);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'con',62);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'ind',41);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'ao',50);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'ico',50);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'aro',51);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'mre',54);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'org',50);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5.5,'ctl',57
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'c',38);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'ex',47);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'con',60);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'ind',37);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'ao',47);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'ico',47);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'aro',48);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'mre',51);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'org',48);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (5,'ctl',54
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'c',35);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'ex',44);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'con',57);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'ind',33);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'ao',44);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'ico',44);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'aro',46);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'mre',49);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'org',45);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4.5,'ctl',51
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'c',31);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'ex',40);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'con',54);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'ind',29);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'ao',41);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'ico',41);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'aro',43);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'mre',46);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'org',42);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (4,'ctl',49
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'c',28);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'ex',37);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'con',52);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'ind',25);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'ao',38);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'ico',39);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'aro',41);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'mre',44);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'org',40);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3.5,'ctl',46
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'c',25);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'ex',34);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'con',49);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'ind',21);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'ao',35);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'ico',36);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'aro',38);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'mre',41);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'org',37);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (3,'ctl',43
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'c',21);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'ex',31);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'con',46);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'ind',17);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'ao',32);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'ico',33);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'aro',36);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'mre',39);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'org',34);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2.5,'ctl',40
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'c',18);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'ex',28);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'con',44);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'ind',13);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'ao',29);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'ico',30);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'aro',33);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'mre',36);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'org',32);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (2,'ctl',38
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'c',14);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'ex',25);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'con',41);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'ind',9);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'ao',25);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'ico',28);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'aro',30);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'mre',34);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'org',29);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1.5,'ctl',35
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'c',11);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'ex',22);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'con',39);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'ind',5);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'ao',22);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'ico',25);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'aro',28);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'mre',32);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'org',26);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (1,'ctl',32
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'c',8);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'ex',19);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'con',36);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'ind',7);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'ao',19);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'ico',22);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'aro',25);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'mre',29);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'org',24);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0.5,'ctl',30
+);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'c',4);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'ex',16);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'con',33);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'ind','N/A');
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'ao',16);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'ico',19);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'aro',23);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'mre',27);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'org',21);
+INSERT INTO fes_lookup(RawScore,Achievement,Percentile) values (0,'ctl',27
+);
