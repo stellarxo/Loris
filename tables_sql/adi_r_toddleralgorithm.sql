e283094832386ea36ece22fb3f3db5c205c7d37f
Rathi Sekaran <sekaranrathi@gmail.com>
2013-06-12 16:29:59 -0400

    adding code changes for new toddler adi-r algorithm

---
+++ tables_sql/adi_r_toddleralgorithm.sql
@@ -0,0 +1,10 @@
+ALTER TABLE adi_r_proband ADD COLUMN social_affect varchar(255) DEFAULT NULL;
+ALTER TABLE adi_r_proband ADD COLUMN repeat_restrict_behavior varchar(255) DEFAULT NULL;
+ALTER TABLE adi_r_proband ADD COLUMN imitation varchar(255) DEFAULT NULL;
+ALTER TABLE adi_r_proband ADD COLUMN reciprocal_interaction varchar(255) DEFAULT NULL;
+ALTER TABLE adi_r_proband ADD COLUMN toddler_summary_diagnosis varchar(255) DEFAULT NULL;
+ALTER TABLE adi_r_subject ADD COLUMN social_affect varchar(255) DEFAULT NULL;
+ALTER TABLE adi_r_subject ADD COLUMN repeat_restrict_behavior varchar(255) DEFAULT NULL;
+ALTER TABLE adi_r_subject ADD COLUMN imitation varchar(255) DEFAULT NULL;
+ALTER TABLE adi_r_subject ADD COLUMN reciprocal_interaction varchar(255) DEFAULT NULL;
+ALTER TABLE adi_r_subject ADD COLUMN toddler_summary_diagnosis varchar(255) DEFAULT NULL;
