f04de4d07c3bd8a43f77e179ded44fcbf5395c5b
Rathi Sekaran <sekaranrathi@gmail.com>
2013-06-19 10:54:27 -0400

    adding validity comment field to instruments

---
+++ tables_sql/AddValidityComment.sql
@@ -0,0 +1,16 @@
+ALTER TABLE adi_r_proband ADD `other_comments` text;
+ALTER TABLE adi_r_subject ADD `other_comments` text;
+ALTER TABLE ados_module1 ADD `other_comments` text;
+ALTER TABLE ados_module2 ADD `other_comments` text;
+ALTER TABLE ados_module3 ADD `other_comments` text;
+ALTER TABLE ados2_module1 ADD `other_comments` text;
+ALTER TABLE ados2_module2 ADD `other_comments` text;
+ALTER TABLE ados2_module3 ADD `other_comments` text;
+ALTER TABLE edi ADD `other_comments` text;
+ALTER TABLE edi2 ADD `other_comments` text;
+ALTER TABLE i3 ADD `other_comments` text;
+ALTER TABLE intervention_history ADD `other_comments` text;
+ALTER TABLE neuro_screen ADD `other_comments` text;
+ALTER TABLE EARLI_interview_PART_1 ADD `other_comments` text;
+ALTER TABLE CBCL ADD `CommentValidity` text;
+ALTER TABLE CBCL ADD `CommentValidity_status` enum('not_answered');
