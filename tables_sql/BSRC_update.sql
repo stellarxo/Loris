ffa00467757c3c0f184f726f6e48415f981639af
Rathi Sekaran <sekaranrathi@gmail.com>
2013-07-04 16:23:50 -0400

    updating instrument after testing

--- tables_sql/BSRC_update.sql
+++ tables_sql/BSRC_update.sql
@@ -2,7 +2,9 @@ ALTER TABLE BSRC ADD COLUMN setting_physicaltherapy enum('office/clinic','home',
 ALTER TABLE BSRC ADD COLUMN setting_speechlangtherapy enum('office/clinic','home','daycare/preschool','other','not_answered');
 ALTER TABLE BSRC ADD COLUMN setting_behaviorintervention enum('office/clinic','home','daycare/preschool','other','not_answered');
 ALTER TABLE BSRC ADD COLUMN setting_otheroptions enum('office/clinic','home','daycare/preschool','other','not_answered');
+ALTER TABLE BSRC ADD COLUMN servicetype_behaviorintervention enum('yes','no','not_answered');
 ALTER TABLE BSRC CHANGE avg_occupationaltherapy avg_occupationaltherapy decimal(10,0);
 ALTER TABLE BSRC CHANGE avg_physicaltherapy_other avg_physicaltherapy_other decimal(10,0);
 ALTER TABLE BSRC CHANGE avg_speechlangtherapy_other avg_speechlangtherapy_other decimal(10,0);
 ALTER TABLE BSRC CHANGE avg_other avg_other decimal(10,0);
+ALTER TABLE BSRC CHANGE q9_continue_nextyr q9_continue_nextyr enum('yes','no','not_applicable','not_answered');
