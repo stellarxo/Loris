7683dcf732a87d3bc4dbdde7ac4614a1ed1bf54c
Rathi Sekaran <sekaranrathi@gmail.com>
2013-08-07 10:01:23 -0400

    updating aims file

--- tables_sql/instrument_update.sql
+++ tables_sql/instrument_update.sql
@@ -1,3 +1,1 @@
-ALTER TABLE aims DROP q37_weightshift_unusuatinedsit;
-ALTER TABLE aims ADD COLUMN q37_weightshift_unusustainedsit enum('credited','observed','not_observed','above_ceiling','not_answered');
-ALTER TABLE BSRC DROP avg_behaviorintervention_other_status;
+ALTER TABLE aims CHANGE COLUMN q37_weightshift_unusuatinedsit q37_weightshift_unsustainedsit enum('credited','observed','not_observed','above_ceiling','not_answered');
