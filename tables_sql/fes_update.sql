de2fd5f2056a0de26bcdb21c7031de2be576d745
Rathi Sekaran <sekaranrathi@gmail.com>
2013-09-09 11:54:10 -0400

    ensuring existing values in backed are copied to new field

--- tables_sql/fes_update.sql
+++ tables_sql/fes_update.sql
@@ -1,1 +1,3 @@
-ALTER TABLE FamilyEnvironmentScales CHANGE COLUMN q82_spontaneous_discussionl q82_spontaneous_discussion enum('true','false','not_answered'); 
+ALTER TABLE FamilyEnvironmentScales ADD COLUMN q82_spontaneous_discussion enum('true','false','not_answered');
+UPDATE FamilyEnvironmentScales SET q82_spontaneous_discussion = q82_spontaneous_discussionl; 
+ALTER TABLE FamilyEnvironmentScales DROP COLUMN q82_spontaneous_discussionl; 
