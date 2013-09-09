0206f9f63bb80f24d4ea9f67997083ff50e80229
Rathi Sekaran <sekaranrathi@gmail.com>
2013-09-09 11:25:43 -0400

    updating number of questions on each page to match the paper form

---
+++ tables_sql/CBCL_update2.sql
@@ -0,0 +1,2 @@
+ALTER TABLE CBCL CHANGE COLUMN parent_mostconcern_1 parent_mostconcern_1 varchar(255);
+ALTER TABLE CBCL CHANGE COLUMN parent_mostconcern_2 parent_mostconcern_2 varchar(255);ALTER TABLE CBCL CHANGE COLUMN parent_mostconcern_3 parent_mostconcern_3 varchar(255);ALTER TABLE CBCL CHANGE COLUMN parent_mostconcern_4 parent_mostconcern_4 varchar(255); 
