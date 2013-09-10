2668ba502bf4a15ece92cc35807335d8255f2528
Rathi Sekaran <sekaranrathi@gmail.com>
2013-09-09 21:19:32 -0400

    adding GRE fields to the mri parameter form

---
+++ tables_sql/mri_parameter_form_update_Sep11_2013.sql
@@ -0,0 +1,2 @@
+ALTER TABLE mri_parameter_form ADD COLUMN gre_accquired enum ('yes','no','not_answered');
+ALTER TABLE mri_parameter_form ADD COLUMN gre_fieldmap enum ('25dir_dti','65dir_dti','bold','not_answered');
