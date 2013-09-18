1dfa0afd1e6838423012a3a44574f25a865b5adb
Rathi Sekaran <sekaranrathi@gmail.com>
2013-09-18 08:19:20 -0400

    updating datatracking form

--- tables_sql/LENA_DataTrackingForm.sql
+++ tables_sql/LENA_DataTrackingForm.sql
@@ -24,3 +24,19 @@ CREATE TABLE `LENA_DataTrackingForm` (
 `Data_dir` varchar(256) default NULL,
 PRIMARY KEY  (`CommentID`)
               );
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Type_1 varchar(20);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN DLP_1  varchar(20);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN ProcessingFile_1 varchar(70);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Timestamp_1 DATETIME;
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Duration_1 varchar(8);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Meaningful_1 varchar(8);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Distant_1 varchar(8);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN TV_1 varchar(8);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN TV_Pct_1 varchar(5);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Noise_1 varchar(8);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Silence_1 varchar(8);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN AWC_Actual_1 varchar(5); 
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN CTC_Actual_1 varchar(3); 
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN CVC_Actual_1 varchar(3);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN AVA_StdScore decimal(3,2);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN AVA_StdScore_Pct varchar(5);
