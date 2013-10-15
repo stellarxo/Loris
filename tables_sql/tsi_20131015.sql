9278a61ba7162d478f01a7c85b4f339d6e36317b
Rathi Sekaran <sekaranrathi@gmail.com>
2013-10-15 12:14:26 -0400

    adding some updates to tsi questions

---
+++ tables_sql/tsi_20131015.sql
@@ -0,0 +1,5 @@
+ALTER TABLE tsi ADD COLUMN lighttherapy_days_proband_status enum('not_answered');
+ALTER TABLE tsi ADD COLUMN breastfed_proband_months_status  enum('not_answered');
+ALTER TABLE tsi ADD COLUMN breastfed_proband_weeks_status enum('not_answered');
+ALTER TABLE tsi ADD COLUMN breastfed_subject_months_status enum('not_answered');
+ALTER TABLE tsi ADD COLUMN breastfed_subject_weeks_status enum('not_answered');
