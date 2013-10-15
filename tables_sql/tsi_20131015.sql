522b0db8a09b67172465dcb80776fd6614d13c7b
Rathi Sekaran <sekaranrathi@gmail.com>
2013-10-15 12:18:20 -0400

    updating sql file to include more fields

--- tables_sql/tsi_20131015.sql
+++ tables_sql/tsi_20131015.sql
@@ -3,3 +3,6 @@ ALTER TABLE tsi ADD COLUMN breastfed_proband_months_status  enum('not_answered')
 ALTER TABLE tsi ADD COLUMN breastfed_proband_weeks_status enum('not_answered');
 ALTER TABLE tsi ADD COLUMN breastfed_subject_months_status enum('not_answered');
 ALTER TABLE tsi ADD COLUMN breastfed_subject_weeks_status enum('not_answered');
+ALTER TABLE tsi CHANGE COLUMN lighttherapy_subject lighttherapy_subject enum('yes','no','not_answered');
+ALTER TABLE tsi ADD COLUMN lighttherapy_days_subject varchar(255);
+ALTER TABLE tsi ADD COLUMN lighttherapy_days_subject_status enum('not_answered');
