c5aaea99bb122012a9a8eef7c38ddcbf57d86383
Rathi Sekaran <sekaranrathi@gmail.com>
2013-11-11 15:14:40 -0500

    updating edit candidate info page

--- tables_sql/candidate_add_caveatflag.sql
+++ tables_sql/candidate_add_caveatflag.sql
@@ -1,2 +1,4 @@
-ALTER TABLE candidate ADD COLUMN flagged_caveatemptor enum('true','false');
-ALTER TABLE candidate ADD COLUMN flagged_info enum('older_sibling_autism','family_excluded');
+ALTER TABLE candidate ADD COLUMN flagged_caveatemptor enum('true','false') DEFAULT 'false';
+ALTER TABLE candidate ADD COLUMN flagged_info enum('older_sibling_autism','family_excluded','other');
+ALTER TABLE candidate ADD COLUMN flagged_other varchar(255);
+ALTER TABLE candidate ADD CoLUMN flagged_other_status enum ('not_answered');
