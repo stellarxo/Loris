900670982e7bbde5b285179df5fe3702543e8396
Rathi Sekaran <sekaranrathi@gmail.com>
2014-01-15 07:47:18 -0500

    adding scoring file

---
+++ tools/PSPQ_Scoring.php
@@ -0,0 +1,76 @@
+<?php
+set_include_path(get_include_path().":../libraries:../../php/libraries:");
+//require_once "NDB_Menu_statistics.class.inc";
+require_once "NDB_Client.class.inc";
+require_once "Utility.class.inc";
+require_once "Database.class.inc";
+//require_once "../../tools/generic_includes.php";
+$client = new NDB_Client();
+$client->makeCommandLine();
+$client->initialize('../config.xml');
+
+$db        =& Database::singleton();
+if (Utility::isErrorX($db)) {
+        print "Could not connect to database: " . $db->getMessage();
+            exit(1);
+}
+$reverse_items = array(1,3,7,9,12,15,16,19,21,23,25,28,30,34,36);
+
+$aloof = array(1,5,9,12,16,18,23,25,27,28,31,36);
+$pragmatic_language = array(2,4,7,10,11,14,17,20,21,29,32,34);
+$rigid = array(3,6,8,13,15,19,22,24,26,30,33,35);
+$candidate_list = $db->pselect("SELECT DISTINCT (s.CandID) FROM flag f JOIN session s ON (s.ID=f.SessionID)
+                                WHERE (Test_name = 'pspq_1' OR Test_name='pspq_2') 
+                                AND f.CommentID NOT LIKE 'DDE%'", array());
+print_r($candidate_list);
+foreach($candidate_list as $cand) {
+    $candidate = $cand['CandID'];
+    $query         = "SELECT f.CommentID FROM flag f JOIN pspq_1 pspq1 
+                      ON (f.CommentID = pspq1.CommentID)
+                      JOIN session s on (s.ID = f.SessionID) WHERE s.CandID =:cand";
+    $pspq_1        = $db->pselectOne($query, array('cand' =>$candidate));
+    if (Utility::isErrorX($pspq_1)) {
+        print "Query has failed to select: ".$pspq_1->getMessage();
+        //        exit(2);
+    }
+    print $pspq_1;
+    $query         = "SELECT f.CommentID FROM flag f JOIN pspq_2 pspq2
+                      ON (f.CommentID = pspq2.CommentID)
+                      JOIN session s on (s.ID = f.SessionID) WHERE s.CandID =:cand";
+    $pspq_2        = $db->pselectOne($query, array('cand' =>$candidate));
+    if (Utility::isErrorX($pspq_2)) {
+        print "Query has failed to select: ".$pspq_2->getMessage();
+        //        exit(2);
+    }
+    $query         = "SELECT CommentID FROM flag f JOIN pspq_score scoring 
+                      ON (f.CommentID = scoring.CommentID)
+                      JOIN session s on (s.ID = f.SessionID) WHERE s.CandID =:cand";
+    $score         = $db->pselectOne($query, array('cand' =>$candidate));
+    if (Utility::isErrorX($score)) {
+        print "Query has failed to select: ".$score->getMessage();
+        //        exit(2);
+    }
+
+    $query         = "SELECT * from pspq_1 WHERE CommentID = :CommentID";
+    $WhereCriteria = array('CommentID'=>$pspq_1);
+    $pspq1_data        = $db->pselectRow($query, $WhereCriteria);
+    if (Utility::isErrorX($pspq1_data)) {
+            print "Query has failed to select: ".$pspq1_data->getMessage();
+    }
+    $query         = "SELECT * from pspq_2 WHERE CommentID = :CommentID";
+    $WhereCriteria = array('CommentID'=>$pspq_2);
+    $papq2_data        = $db->pselectRow($query, $WhereCriteria);
+    if (Utility::isErrorX($pspq2_data)) {
+            print "Query has failed to select: ".$pspq2_data->getMessage();
+    }
+}
+function reverscores($values) {
+    $reverse_items = array(1,3,7,9,12,15,16,19,21,23,25,28,30,34,36);
+    $participant = array('respondent'=>'q','informant'=>'qi');
+    foreach($participant as $key=>$val) {
+       foreach($reverse_items as $qnum) {
+               
+       }    
+    }
+}
+?>
