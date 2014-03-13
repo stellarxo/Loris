c6ec793864a3f1eef66674ce16d04146fe14a05d
Rathi <sekaranrathi@gmail.com>
2014-03-13 12:42:07 -0400

    adding qc_run script that calls Qc_Checks class per site. Adding NDB  factory class so that the qc_run script can be run from the project tools directory

---
+++ tools/qc_run.php
@@ -0,0 +1,26 @@
+<?php
+set_include_path(get_include_path().":../libraries:../../php/libraries:");
+require_once "NDB_Client.class.inc";
+require_once "QC_Checks.class.inc";
+require_once "NDB_Config.class.inc";
+
+$config =& NDB_Config::singleton("../config.xml");
+$config->load("../config.xml");
+$client = new NDB_Client();
+$client->makeCommandLine();
+$client->initialize('../config.xml');
+
+// get a Database connection
+$dbConfig = $config->getSetting('database');
+$DB = new Database;
+$result = $DB->connect($dbConfig['database'], $dbConfig['username'], $dbConfig['password'], $dbConfig['host'], false);
+if(PEAR::isError($result)) {
+        die("Could not connect to database: ".$result->getMessage());
+}
+$sites =array('2','3','4','5'); 
+foreach($sites as $site) {
+$run = new QC_Checks($site);
+ $run->run();
+}
+
+?>
