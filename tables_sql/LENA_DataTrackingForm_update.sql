3b1e10df6ae2dee879d46d716dfaa870f995067d
Rathi <sekaranrathi@gmail.com>
2014-03-27 16:46:54 -0400

    adding 3 more uploaders for .its files for LENA_datatracking form

---
+++ tables_sql/LENA_DataTrackingForm_update.sql
@@ -0,0 +1,4 @@
+ALTER TABLE LENA_DataTrackingForm CHANGE COLUMN Uploaded_file_its Uploaded_file_its1 varchar(255);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Uploaded_file_its2 varchar(255);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Uploaded_file_its3 varchar(255);
+ALTER TABLE LENA_DataTrackingForm ADD COLUMN Uploaded_file_its4 varchar(255);
