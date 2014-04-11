b6fd44262d777ca861e9a31767133f14d2d28db9
Rathi <sekaranrathi@gmail.com>
2014-04-11 15:52:40 -0400

    changing q8 to be a multi select option

--- tables_sql/peer_socialcontact_update2.sql
+++ tables_sql/peer_socialcontact_update2.sql
@@ -19,3 +19,8 @@ ALTER TABLE peer_socialcontact ADD COLUMN `q10d_autism_effect_younger` varchar(2
 ALTER TABLE peer_socialcontact ADD COLUMN `q10e_autism_effect_older` varchar(255) default NULL;
 ALTER TABLE peer_socialcontact ADD COLUMN `q11_comments` text default NULL;
 ALTER TABLE peer_socialcontact CHANGE COLUMN total_playmates total_playmates enum('0','1','2','3','4','5') ;
+ALTER TABLE peer_socialcontact CHANGE COLUMN q8_specialneeds_playmate1 q8_specialneeds_playmate1 varchar(255);
+ALTER TABLE peer_socialcontact CHANGE COLUMN q8_specialneeds_playmate2 q8_specialneeds_playmate2 varchar(255);
+ALTER TABLE peer_socialcontact CHANGE COLUMN q8_specialneeds_playmate3 q8_specialneeds_playmate3 varchar(255);
+ALTER TABLE peer_socialcontact CHANGE COLUMN q8_specialneeds_playmate4 q8_specialneeds_playmate4 varchar(255);
+ALTER TABLE peer_socialcontact CHANGE COLUMN q8_specialneeds_playmate5 q8_specialneeds_playmate5 varchar(255);
