46e756a4deb4a33f72ed6f04f35f41fb2fdbc269
stellarxo <stellajhlee@hotmail.com>
2015-06-17 11:27:28 -0400

    Created script to map already entered answers to drop down options

---
+++ tools/Redmine5765.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stella
+ * Date: 15-06-17
+ * Time: 10:17 AM
+ */
+
+set_include_path(get_include_path().":../libraries:../../php/libraries:");
+require_once "NDB_Client.class.inc";
+require_once"Utility.class.inc";
+require_once"Database.class.inc";
+
+$client = new NDB_Client();
+$client->makeCommandLine();
+$client->initialize('../config.xml');
+
+$db =& Database::singleton();
+
+$query = $db->pselect("SELECT relationship_to_child, relationship_to_child_specify, relationship_to_child_status FROM csbs", array());
+
+foreach ($query as $row) {
+
+    // Mother
+    if ($row['relationship_to_child'] == 'mother' or $row['relationship_to_child'] == 'Mother' or $row['relationship_to_child'] == 'Mom') {
+        $query = $db->update('csbs', array($row['relationship_to_child'] => '1_Mother'));
+    }
+
+    // Father
+    elseif ($row['relationship_to_child'] == 'father' or $row['relationship_to_child'] == 'Father' or $row['relationship_to_child'] == 'Dad' or $row['relationship_to_child'] == 'dad') {
+        $query = $db->update('csbs', array($row['relationship_to_child'] => '1_Father'));
+    }
+
+    // Not Answered
+    elseif ($row['relationship_to_child_status'] == 'not_answered' or $row['relationship_to_child'] == 'No response' or $row['relationship_to_child'] == 'NA' or $row['relationship_to_child'] == 'n/a') {
+        $query = $db->update('csbs', array($row['relationship_to_child'] => 'not_answered'));
+    }
+
+    // Other
+    elseif ($row['relationship_to_child'] != "" && $row['relationship_to_child'] != NULL) {
+        $response = $row['relationship_to_child'];
+        $query = $db->update('csbs', array($row['relationship_to_child'] => '3_Other'));
+        $query = $db->update('csbs', array($row['relationship_to_child_specify'] => $response));
+    }
+
+}
+
+// Run following query to remove column
+// ALTER TABLE csbs DROP COLUMN relationship_to_child_status
+
+
+?>
