7f7caaf310bb829955fe0c19cedbd17bf043be04
Gregory Luneau <gregory.luneau@gmail.com>
2016-01-27 10:30:08 -0500

    recent changes to production

--- tools/ndar_query.php
+++ tools/ndar_query.php
@@ -3,7 +3,7 @@ set_include_path(get_include_path().":../libraries:../../php/libraries:");
 require_once '../../vendor/autoload.php';
 require_once "Database.class.inc";
 require_once "Utility.class.inc";
-require_once "NDAR_Release_2015.class.inc";
+require_once "NDAR_Release_2016.class.inc";
 
 $client = new NDB_Client();
 $client->makeCommandLine();
@@ -11,22 +11,17 @@ $client->initialize('../config.xml');
 
 $db        =& Database::singleton();
 
-//$IBIS = array('m_chat_proband');
-$IBIS = array ('adi_r_proband', 'aosi','aosi','csbs','edi','fyi','ibq_r',
-    'm_chat_proband','macarthur_words_gestures','mullen','prefrontal_task','rbs_r',
-    'scq_proband','scq_subject','seq','vineland_proband',
-    'vineland_subject','charge');
+//$IBIS = array('charge');
+$IBIS = array ('adi_r_proband', 'aosi','aosi','csbs','edi','fyi','ibq_r','m_chat_proband','macarthur_words_gestures','mullen','prefrontal_task','rbs_r','scq_proband','scq_subject','seq','vineland_proband','vineland_subject','charge');
 foreach($IBIS as $instrument) {
-    $run = new NDAR_Release_2015 ($instrument, 1);
+    $run = new NDAR_Release_2016 ($instrument, 1);
     $run->run();
 }
 
-//$IBIS2 = array('m_chat_proband');
-$IBIS2 = array('adi_r_proband','aosi','csbs','head_measurements_subject',
-    'ibq_r','macarthur_words_gestures','m_chat_proband','scq_proband','mullen',
-    'rbs_r','vineland_proband', 'charge');
+//$IBIS2 = array('charge');
+$IBIS2 = array('adi_r_proband','aosi','csbs','head_measurements_subject','ibq_r','macarthur_words_gestures','m_chat_proband','scq_proband','mullen','rbs_r','vineland_proband', 'charge');
 foreach($IBIS2 as $instrument) {
-    $run = new NDAR_Release_2015 ($instrument, 2);
+    $run = new NDAR_Release_2016 ($instrument, 2);
     $run->run();
 
 }
