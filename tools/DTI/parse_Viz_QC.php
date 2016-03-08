2cbbfd1ed459898e453ca3325b746f6de695505c
Gregory Luneau <gregory.luneau@gmail.com>
2016-03-08 10:00:48 -0500

    parse for DTI prep verification

--- tools/DTI/parse_Viz_QC.php
+++ tools/DTI/parse_Viz_QC.php
@@ -12,8 +12,10 @@
 //ini_set('display_startup_errors', TRUE);
 
 set_include_path(get_include_path().":../../libraries:../../../php/libraries:");
-require('../../externals/spreadsheet-reader/php-excel-reader/excel_reader2.php');
-require('../../externals/spreadsheet-reader/SpreadsheetReader.php');
+require_once __DIR__ . "/../../../vendor/autoload.php";
+//require('../../externals/spreadsheet-reader/php-excel-reader/excel_reader2.php');
+//require('../../externals/spreadsheet-reader/SpreadsheetReader.php');
+//require_once "NDB_Page.class.inc";
 require_once "NDB_Client.class.inc";
 require_once "Utility.class.inc";
 
@@ -22,31 +24,61 @@ $client->makeCommandLine();
 $client->initialize('../../config.xml');
 $DB = Database::singleton();
 
-$Reader = new SpreadsheetReader($argv[1]);
-$Reader->ChangeSheet($argv[2]);
+//$Reader = new SpreadsheetReader($argv[1]);
+//$Sheets = $Reader -> Sheets();
+//print_r($Sheets);
+//$Reader->ChangeSheet($argv[2]);
 
 
+$file = $argv[1];
+$fp = fopen($file, "r");
+$headers = fgetcsv($fp);
+$headers = array_slice($headers, 3, 36);
 
-foreach ($Reader as $Key => $excel_row)
+$i=2;
+
+// foreach ($Reader as $Key => $excel_row)
+while($excel_row = fgetcsv($fp))
 {
-    print $Key . ", ";
-    $db_files = $DB->pselectRow("SELECT * FROM files where File like '%" . $excel_row[2] . "%' and SeriesUID is not null", array());
-    //print_r($excel_row);
+    //print $Key . ": ". "SELECT * FROM files where File like '%" . $excel_row[11] . "_DTIPrepReg_001.mnc'" ;  
+    // _DTIPrepReg_001.mnc 
+    // "SELECT * FROM files where File like '%" . $excel_row[2] . "%' and SeriesUID is not null"
+    // "SELECT * FROM files where File like '%" . $excel_row[2] . "_DTIPrepReg_001.mnc'"
+
+    // column 40 is new file name 
+    //$name_prep = $excel_row[40] . "_DTIPrepReg_001.mnc";
+    //$name_wild = $excel_row[40] . ".mnc";
+
+
+    $name_prep = $excel_row[2] . "_DTIPrepReg_001.mnc";
+    $name_wild = $excel_row[2] . ".mnc";
+    
+    $db_files_prep = $DB->pselectRow("SELECT * FROM files where File like '%" . $name_prep . "'", array());
+    $db_files_wild = $DB->pselectRow("SELECT * FROM files where File like '%" . $name_wild . "%'", array());    
+//print_r($excel_row);
+    
+    if ($db_files_prep) { 
+        $db_files = $db_files_prep; 
+        $name_file = $name_prep;
+    } else  { 
+        $db_files = $db_files_wild; 
+        $name_file = $name_wild;
+    }
     
     if ($db_files) 
     {
         // file found proceed
         //print_r($db_files);
-        print "$excel_row[2], $excel_row[1], $db_files[FileID], ";
+        // print "$excel_row[2], $excel_row[1], $db_files[FileID], ";
 
         // files_qcstatus is already populated
         $db_files_qcstatus = $DB->pselectRow("SELECT * FROM files_qcstatus where FileID=:FID", array("FID" => $db_files["FileID"]));
         if (!$db_files_qcstatus)
         {
-            print "no db_files_qcstatus.\n";
+            print $i . ":" . $name_file . " no db_files_qcstatus\n";
         }
         else {
-            print "db_files_qcstatus found.\n";
+            print $i . ":" . $name_file . " yes db_files_qcstatus\n";
         }
         //print_r($db_files_qcstatus);
         
@@ -74,12 +106,13 @@ foreach ($Reader as $Key => $excel_row)
     else
     {
         // file not found, explain
-        print "$excel_row[2], File not found in the db, can't insert QC data.\n";
+        print $i . ":" . $name_file . " File not_found_in_db\n";
     }    
     
     //print ".";
     //$handle = fopen ("php://stdin","r");
     //$line = fgets($handle);
+    $i++;
 }
 	
 
